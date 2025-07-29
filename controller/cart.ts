5a5b398e64b70a6ae05036179631a16f26d3f525
Dev-hamzaa <xzerefff@gmail.com>
2025-07-29 18:29:33 +0500

    get & update userCart

--- controller/cart.ts
+++ controller/cart.ts
@@ -23,7 +23,62 @@ const createCart = async (req: Request, res: Response, next: NextFunction) => {
         return errorHandler(500, 'internal server error', next)
     }
 }
+const updateCart = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const { userId, products } = req.body;
+        if (!userId || !products) {
+            return errorHandler(400, "Please provide the user id and products", next)
+        }
+        const updatedCart = await Cart.findByIdAndUpdate(
+            {
+                _id: id
+            },
+            {
+                userId: userId,
+                products: products
+            },
+            {
+                new: true
+            }
+        )
+        if (updatedCart) {
+            return res.status(200).json({
+                success: true,
+                message: "Cart updated successfully",
+                data: updatedCart
+            })
+        }
+        return errorHandler(400, "Cart not updated", next)
+    } catch (error) {
+        return errorHandler(500, "internal server Error", next)
+    }
+}
+
+const getUserCart = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { userId } = req.body;
+        if (!userId) {
+            return errorHandler(400, "Please provide the user id", next)
+        }
+        const userCart = await Cart.findOne({ userId: userId })
+        if (userCart) {
+            return res.status(200).json({
+                success: true,
+                message: "Cart fetched successfully",
+                data: userCart
+            })
+        }
+        return errorHandler(400, "Cart not found", next)
+
+    } catch (error) {
+        return errorHandler(500, "internal server Error", next)
+    }
+}
+
 
 export const cartController = {
-    createCart
+    createCart,
+    getUserCart,
+    updateCart
 }
